6c2c5571dd2044e234a8f2db790c2e22dd1ceb03
PC <PC@DESKTOP-HAIIK4K>
2025-02-18 19:02:07 +0530

    Fifth Commit

---
+++ AutomationCourse/src/test/java/seleniumbasics/LoginTest.java
@@ -0,0 +1,13 @@
+package seleniumbasics;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	@Test
+	public void verifyUserLoginWithValidUsernameAndValidPassword()
+	{
+		
+	}
+
+}
--- AutomationCourse/src/test/java/seleniumbasics/TestNGAnnotations.java
+++ AutomationCourse/src/test/java/seleniumbasics/TestNGAnnotations.java
@@ -1,6 +1,16 @@
 package seleniumbasics;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
 
 public class TestNGAnnotations {
 	@Test
@@ -9,4 +19,51 @@ public class TestNGAnnotations {
 		System.out.println("This is a test case");
 	}
 	
+	@BeforeMethod
+	public void beforeMethod()
+	{
+		System.out.println("Before Method");
+	}
+	
+	@AfterMethod
+	public void afterMethod()
+	{
+		System.out.println("After Method");
+	}
+	
+	@BeforeClass
+	public void beforeClass()//executed before at tes
+	{
+		System.out.println("Before Class");
+	}
+	
+	@AfterClass
+	public void afterClass()
+	{
+		System.out.println("After Class");
+	}
+	
+	@BeforeTest
+	public void beforeTest()
+	{
+		System.out.println("Before Test");
+	}
+	
+	@AfterTest
+	public void afterTest()
+	{
+		System.out.println("After Test");
+	}
+	
+	@BeforeSuite
+	public void beforeSuite()
+	{
+		System.out.println("Before suit");
+	}
+	
+	@AfterSuite
+	public void afterSuite()
+	{
+		System.out.println("After suite");
+	}
 }
